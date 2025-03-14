f40daacab93a69374014c94e59391bbaa31115e2
Puneet Koundal <puneetkoundal707@gmail.com>
2025-03-14 21:02:47 +0530

    engine.py

--- engine.py
+++ engine.py
@@ -1,20 +1,104 @@
+import math 
 class Value :
-    def __init__(self,data,_children=() , _op = ' '):
+    def __init__(self,data,_children=() , _op = ' ' ):
         self.data = data
         self._prev = set(_children)
         self._op = _op 
         self.grad = 0
+        self._backward = lambda : None 
+        
 
     def __repr__(self):
         return f" >> {self.data}"
     
     def __add__(self,other):    #interally it goes like a.__add__(b)
         out = Value(self.data + other.data ,(self,other) , '+')
+
+        def _backward():
+            self.grad = 1.0 * out.grad
+            other.grad = 1.0 * out.grad
+        out._backward = _backward
         return out
     
     def __mul__(self,other):
         out = Value(self.data * other.data , (self,other) , '*')
+
+        def _backward() :
+            self.grad = 1.0 * out.grad 
+            other.grad = 1.0 * out.grad
+        out._backward = _backward
         return out
+    
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2*x) - 1) / (math.exp(2*x) + 1)
+        out = Value(t, (self,), 'tanh') 
+
+        def _backward():
+            self.grad = (1 - t*t) * out.grad
+        out._backward = _backward
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+        out = Value(self.data**other, (self,), f'**{other}')
+
+        def _backward():
+            self.grad += (other * self.data**(other-1)) * out.grad
+        out._backward = _backward
+
+        return out\
+        
+    def relu(self):
+        out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
+
+        def _backward():
+            self.grad += (out.data > 0) * out.grad
+        out._backward = _backward
+
+        return out
+    
+    
+    def backward(self):
+
+        # topological order all of the children in the graph
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        # go one variable at a time and apply the chain rule to get its gradient
+        self.grad = 1
+        for v in reversed(topo):
+            v._backward()
+        
+    def __neg__(self): # -self
+        return self * -1
+
+    def __radd__(self, other): # other + self
+        return self + other
+
+    def __sub__(self, other): # self - other
+        return self + (-other)
+
+    def __rsub__(self, other): # other - self
+        return other + (-self)
+
+    def __rmul__(self, other): # other * self
+        return self * other
+
+    def __truediv__(self, other): # self / other
+        return self * other**-1
+
+    def __rtruediv__(self, other): # other / self
+        return other * self**-1
+
+    def __repr__(self):
+        return f"Value(data={self.data}, grad={self.grad})"
         
 
     
